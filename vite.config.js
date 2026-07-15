3a2ad6225a7489d5b3c6f56bf204026f1729b634
Camilo Zapata <camilo_zapata@cotecnova.edu.co>
2026-07-15 04:20:20 -0500

    Add fontsource dependencies and update app styles

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
             fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
             ],
         }),
         tailwindcss(),
